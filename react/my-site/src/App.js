a1cbecd57bf10856cbb749b102e1f9fc240560ad
maria <maria@clickdb.com.br>
2022-10-14 10:05:33 -0300

    feat: grid adicionado

--- react/my-site/src/App.js
+++ react/my-site/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import { Table } from './components/table/table';
 import tbl from './components/table/table.json'
-import WhatIDoElement from './components/whatIDoElements';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <Table elements={tbl}/>
-      </header>
-    </div>
+    return (
+        <div className='App'>
+            <Table elements={tbl}/>
+        </div>
   );
 }
 
